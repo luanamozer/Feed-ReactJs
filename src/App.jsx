27e2b672981068fcd7fcc7a4d20dc2f3449d73c2
Luana Mozer <luanamozzer1996@gmail.com>
2022-06-14 17:36:30 -0300

    finalizando componente header

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Header } from "./components/Header";
 import "./global.css";
 export function App() {
   return (
-    <>
+    <div>
       <Header />
       <Post
         author="Luana"
@@ -12,6 +12,6 @@ export function App() {
       />
 
       <Post author="Esther Gomes" content="Esposa da autora 1" />
-    </>
+    </div>
   );
 }
