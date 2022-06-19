fa81f50367d3052f2e72ced2546f10611f383336
Luana Mozer <luanamozzer1996@gmail.com>
2022-06-19 19:53:24 -0300

    finalizando a sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import { Post } from "./Post";
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 
+import styles from "./App.module.css";
 import "./global.css";
+
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Luana"
-        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque, tempore. A, consectetur accusantium. Aliquam fugit dolorem odit perferendis facere qui. Quia assumenda natus quidem consectetur facilis eum magnam voluptas consequatur?"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Luana"
+            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque, tempore. A, consectetur accusantium. Aliquam fugit dolorem odit perferendis facere qui. Quia assumenda natus quidem consectetur facilis eum magnam voluptas consequatur?"
+          />
 
-      <Post author="Esther Gomes" content="Esposa da autora 1" />
+          <Post author="Esther Gomes" content="Esposa da autora 1" />
+        </main>
+      </div>
     </div>
   );
 }
